65ed17c9bfaa0922d52f9a0b2b04bb6cb9783436
ushasree-mangi <ushasree2022@gmail.com>
2024-09-05 12:26:52 +0530

    new code

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express = require("express");
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const bcrypt=require('bcrypt')
 const app = express();
+
+const jwt=require("jsonwebtoken")
+const {v4}=require("uuid")
+const uuidv4=v4
+
 
 app.use(express.json())
 
@@ -42,4 +48,89 @@ app.get("/products/", async (request, response) => {
   const productsArray = await db.all(getProductsQuery);
   response.send(productsArray);
 });
+
+
+
+
+
+
+
+
+
+//Register Api
+  app.post("/register/", async (request, response) => {
+    
+    const {username,password}=request.body
+    const id=uuidv4()
+    console.log(`api log : id :${id} username:${username} password ${password}`)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
+    const dbUser = await db.get(selectUserQuery);
+    if (dbUser === undefined) {
+      const createUserQuery = `
+        INSERT INTO 
+          users (id ,username, password) 
+        VALUES 
+          ('${id}',
+            '${username}', 
+            
+            '${hashedPassword}'
+            
+          )`;
+      const dbResponse = await db.run(createUserQuery);
+      const newUserId = dbResponse.lastID;
+      response.send(`Created new user with ${newUserId}`);
+    } else {
+      response.status = 400;
+      response.send("User already exists");
+    }
+  }); 
+ 
+//Login Api
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+//middleware Api 
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.email = payload.email;
+        next();
+      }
+    });
+  }
+};
+
 
