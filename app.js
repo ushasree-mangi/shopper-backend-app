445c4ec7b02d916659332b81d46d70af6b52e829
ushasree-mangi <ushasree2022@gmail.com>
2024-09-08 17:42:51 +0530

    new

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const {v4}=require("uuid")
 const uuidv4=v4
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow localhost:3000
+  origin: 'https://usha-sree-mangi-shopper-app.vercel.app/', // Allow localhost:3000
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include OPTIONS
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow cookies and credentials
