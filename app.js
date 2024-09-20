09d2772525dc0527da28ac3e5ee3d74d9b3b8b04
ushasree-mangi <ushasree2022@gmail.com>
2024-09-20 16:01:43 +0530

    delete cart item

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
 
 
 
-/*const corsOptions = {
+const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
@@ -33,17 +33,14 @@ const allowedOrigins = [
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow cookies and credentials
-}; */
+}; 
 
 
 app.use(express.json())
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-//app.options('*', cors(corsOptions));
-//remove this when deploying 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.options('*', cors(corsOptions));
+
 
 const PORT =  process.env.PORT || 4000
 
